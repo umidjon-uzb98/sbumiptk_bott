4fbd7c217517a9078deedea5dd38c99132a9a618
lordu98 <u.makhmudov211326@gmail.com>
2022-09-06 20:21:07 +0500

    new

--- src/main/java/FindWithPassportNumberBot.java
+++ src/main/java/FindWithPassportNumberBot.java
@@ -1,44 +1,29 @@
-import org.glassfish.grizzly.http.server.util.RequestUtils;
-import org.glassfish.grizzly.streams.Input;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.GetFile;
 import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
+import org.telegram.telegrambots.meta.api.objects.File;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import javax.xml.bind.DatatypeConverter;
-import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class FindWithPassportNumberBot extends TelegramLongPollingBot {
-
 
     @Override
     public void onUpdateReceived(Update update) {
         Message message = update.getMessage();
         String passNumber = message.getText();
 
-        URL url = null;
         try {
-//            url = new URL("\u202AC:\\Users\\Sabr\\Desktop\\pdf_files\\" + passNumber + ".pdf");
-            url = new URL("C:\\Users\\Sabr\\Desktop\\pdf_files\\pdf2.pdf");
-            InputStream inputStream = url.openStream();
-
-
             SendDocument sendDoc = new SendDocument();
             sendDoc.setChatId(message.getChatId());
-            InputFile inputFile = new InputFile(String.valueOf(inputStream));
             sendDoc.setDocument(inputFile);
-
             execute(sendDoc);
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
     @Override
@@ -50,14 +35,5 @@ public class FindWithPassportNumberBot extends TelegramLongPollingBot {
     public String getBotToken() {
         return "";
     }
-    private void sendFile(Long chatId, InputStream is, String name){
-        SendDocument outMess = new SendDocument();
-        outMess.setChatId(chatId);
-//        outMess.setNewDocument(name, is);
-//        try{
-//            sendDocument(outMess);
-//        } catch (TelegramApiException e){
-//            e.printStackTrace();
-//        }
-    }
+
 }
---
+++ src/main/java/TestUn.java
@@ -0,0 +1,66 @@
+import org.glassfish.grizzly.http.server.util.RequestUtils;
+import org.glassfish.grizzly.streams.Input;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.GetFile;
+import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
+import org.telegram.telegrambots.meta.api.objects.File;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import javax.xml.bind.DatatypeConverter;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Path;
+
+public class TestUn extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        String passNumber = message.getText();
+
+        GetFile getFile = new GetFile(message.getDocument().getFileId());
+
+        try {
+            File tgFile = execute(getFile);
+            String fileUrl = tgFile.getFileUrl(getBotToken());
+                System.out.println(fileUrl);
+
+            URL url = new URL(fileUrl);
+            InputStream inputStream = url.openStream();
+            InputFile inputFile = new InputFile(String.valueOf(inputStream));
+
+            SendDocument sendDoc = new SendDocument();
+            sendDoc.setChatId(message.getChatId());
+            sendDoc.setDocument(inputFile);
+            execute(sendDoc);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "sbumiptk_info_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "5685358111:AAGE7B9_RX-SuYkCO85V3NM9uCgyPjcMXhw";
+    }
+
+//    private void sendFile(Long chatId, InputStream is, String name) {
+//        SendDocument outMess = new SendDocument();
+//        outMess.setChatId(chatId);
+//        outMess.setNewDocument(name, is);
+//        try{
+//            sendDocument(outMess);
+//        } catch (TelegramApiException e){
+//            e.printStackTrace();
+//        }
+//    }
+}
